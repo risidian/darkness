45367964298b06fb10811cf6683543b7277da474
Mayce Gray <mayce.developer@gmail.com>
2018-10-28 12:27:18 +0800

    WebAPI: Add database connection

---
+++ Darkness.WebAPI/Models/DarknessContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Darkness.WebAPI.Models
+{
+    public partial class DarknessContext : DbContext
+    {
+        public DarknessContext()
+        {
+        }
+
+        public DarknessContext(DbContextOptions<DarknessContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=COMPUTER\\DARKNESS;Database=Darkness;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>(entity =>
+            {
+                entity.Property(e => e.Creation).HasColumnType("datetime");
+
+                entity.Property(e => e.EmailAddress)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Guid)
+                    .HasColumnName("GUID")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TimeZone)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Xp).HasColumnName("XP");
+            });
+        }
+    }
+}
---
+++ Darkness.WebAPI/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Darkness.WebAPI.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int Age { get; set; }
+        public string TimeZone { get; set; }
+        public string Guid { get; set; }
+        public int? Xp { get; set; }
+        public int? PlayerLevel { get; set; }
+        public DateTime? Creation { get; set; }
+    }
+}
