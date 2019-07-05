0cf3a2dc00bde8cb308961d3ede5c2e851d0b85b
Mayce Gray <GrayM@milne.com.au>
2019-07-05 18:51:00 +0800

    Add: pvp button

--- Darkness.Android/Resources/Resource.Designer.cs
+++ Darkness.Android/Resources/Resource.Designer.cs
@@ -82,16 +82,16 @@ namespace Darkness.Android
 			public const int Mute = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int settingMode = 2130837511;
+			public const int PlayerVsPlayer = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int smithMode = 2130837512;
+			public const int settingMode = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int Splash = 2130837513;
+			public const int smithMode = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int storyMode = 2130837514;
+			public const int Splash = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
 			public const int StoryModeButton = 2130837515;
