808fce4547ab5d692d522e6b9378dd481055071b
Mayce Gray <GrayM@milne.com.au>
2019-07-10 13:14:25 +0800

    Add: Animation Class

---
+++ Darkness.Android/Animations/AnimationFrame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework;
+
+namespace Darkness.Android.Animations
+{
+    class AnimationFrame
+    { 
+        public Rectangle SourceRectangle { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
+
