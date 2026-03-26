4be2ec321b37319ead833ec9dbb258266e91abc6
risidian <maycegray+git@gmail.com>
2026-03-26 10:56:43 +0800

    Fix project metadata: add Platforms directory with Windows and Android boilerplate

---
+++ Darkness.MAUI/Platforms/Android/MainActivity.cs
@@ -0,0 +1,11 @@
+using Android.App;
+using Android.Content.PM;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace Darkness.MAUI;
+
+[Activity(Theme = "@style/Maui.MainTheme.NoActionBar", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
