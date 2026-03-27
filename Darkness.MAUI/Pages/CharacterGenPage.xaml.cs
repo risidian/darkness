46c739a70d88478cfac122dc73e1cd6ad49cce26
risidian <maycegray+git@gmail.com>
2026-03-27 09:48:45 +0800

    feat: trigger initial sprite preview on character gen page load

--- Darkness.MAUI/Pages/CharacterGenPage.xaml.cs
+++ Darkness.MAUI/Pages/CharacterGenPage.xaml.cs
@@ -4,10 +4,19 @@ namespace Darkness.MAUI.Pages
 {
     public partial class CharacterGenPage : ContentPage
     {
+        private readonly CharacterGenViewModel _viewModel;
+
         public CharacterGenPage(CharacterGenViewModel viewModel)
         {
             InitializeComponent();
-            BindingContext = viewModel;
+            _viewModel = viewModel;
+            BindingContext = _viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await _viewModel.UpdatePreviewAsync();
         }
     }
 }
