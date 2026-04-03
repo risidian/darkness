5da22adc8f53293c9bc8ad2230ba6dd793afd7a5
risidian <maycegray+git@gmail.com>
2026-04-03 15:28:47 +0800

    feat: add DialogueChoice model to support interactive dialogue

---
+++ Darkness.Core/Models/DialogueChoice.cs
@@ -0,0 +1,8 @@
+namespace Darkness.Core.Models
+{
+    public class DialogueChoice
+    {
+        public string Text { get; set; } = string.Empty;
+        public string NextQuestId { get; set; } = string.Empty;
+    }
+}
--- Darkness.Core/Models/DialogueData.cs
+++ Darkness.Core/Models/DialogueData.cs
@@ -6,5 +6,6 @@ namespace Darkness.Core.Models
     {
         public string Speaker { get; set; } = string.Empty;
         public List<string> Lines { get; set; } = new();
+        public List<DialogueChoice> Choices { get; set; } = new();
     }
 }
