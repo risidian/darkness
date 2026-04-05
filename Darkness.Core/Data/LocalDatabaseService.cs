af7ea721ea7a6aad7617fd427d77d540823b62f9
risidian <maycegray+git@gmail.com>
2026-04-05 19:37:04 +0800

    feat: add OpenDatabase accessor to LocalDatabaseService

--- Darkness.Core/Data/LocalDatabaseService.cs
+++ Darkness.Core/Data/LocalDatabaseService.cs
@@ -1,4 +1,5 @@
 using Darkness.Core.Interfaces;
+using LiteDB;
 using System.IO;
 
 namespace Darkness.Core.Data
@@ -6,10 +7,15 @@ namespace Darkness.Core.Data
     public class LocalDatabaseService
     {
         private readonly IFileSystemService _fileSystem;
+        private readonly string _dbPath;
 
         public LocalDatabaseService(IFileSystemService fileSystem)
         {
             _fileSystem = fileSystem;
+            string directory = _fileSystem.AppDataDirectory;
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+            _dbPath = Path.Combine(directory, "Darkness.db");
         }
 
         public string GetLocalFilePath(string filename)
@@ -19,8 +25,9 @@ namespace Darkness.Core.Data
             {
                 Directory.CreateDirectory(directory);
             }
-
             return Path.Combine(directory, filename);
         }
+
+        public LiteDatabase OpenDatabase() => new LiteDatabase(_dbPath);
     }
 }
