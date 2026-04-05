b610ff693e53475f5026cd3f5bcf7c282ec41a5d
risidian <maycegray+git@gmail.com>
2026-04-05 20:07:45 +0800

    fix: consolidate DialogueChoice into DialogueData.cs (DialogueChoice.cs deleted)

--- Darkness.Core/Models/DialogueData.cs
+++ Darkness.Core/Models/DialogueData.cs
@@ -2,6 +2,13 @@ using System.Collections.Generic;
 
 namespace Darkness.Core.Models
 {
+    public class DialogueChoice
+    {
+        public string Text { get; set; } = string.Empty;
+        public string NextQuestId { get; set; } = string.Empty;
+        public int MoralityImpact { get; set; } = 0;
+    }
+
     public class DialogueData
     {
         public string Speaker { get; set; } = string.Empty;
