c025a69d3d401e342d3668eddecd09816c72a54a
risidian <maycegray+git@gmail.com>
2026-04-14 08:29:21 +0800

    feat: update IWeaponSkillService interface with GetAvailableSkills and GetEquippedSkills

--- Darkness.Core/Interfaces/IWeaponSkillService.cs
+++ Darkness.Core/Interfaces/IWeaponSkillService.cs
@@ -6,4 +6,6 @@ namespace Darkness.Core.Interfaces;
 public interface IWeaponSkillService
 {
     List<Skill> GetSkillsForWeapon(string? weaponType, string? offHandType, string? shieldType, List<string>? unlockedTalentIds = null);
+    List<Skill> GetAvailableSkills(Character character);
+    List<Skill> GetEquippedSkills(Character character);
 }
